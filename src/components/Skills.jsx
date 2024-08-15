93f3cb7c0f10c0728140fef606dac9b87b8a2cb7
shun-naganuma <129668177+shun-naganuma@users.noreply.github.com>
2024-08-15 20:50:23 +0900

    5-1まで完了

---
+++ src/components/Skills.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
+export const Skills = () => {
+useEffect(() => { axios.get('https://api.github.com/users/shun-naganuma/repos').then((response) => console.log(response)) }, []);
+
+    return (
+        <div id="skills">
+            <div className="container">
+                <div className="heading">
+                    <h2>Skills</h2>
+                </div>
+                <div className="skills-container">
+                </div>
+            </div>
+        </div>
+    );
+};
